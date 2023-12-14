04647c356246e5a3655718ed7dcbbffe66686bc2
Long <thanhlongh124@gmail.com>
2023-12-14 23:04:31 +0700

    cart build

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheSippingStation.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GameStore.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
