a24f41e2ca76418fe7f24152eedb7676b41cef82
Long <thanhlongh124@gmail.com>
2023-12-25 12:40:51 +0700

    requirements

--- cart/controller.py
+++ cart/controller.py
@@ -8,14 +8,8 @@ class CartController:
         
 @dataclass
 class OrderController:
-    undo_stack: list[Command] = field(default_factory=list) 
     
     def execute(self, command:Command):
         command.execute()
-        self.undo_stack.append(command)
-    def undo(self):
-        if not self.undo_stack:
-            return
-        command = self.undo_stack.pop()
-        command.undo()
+   
         
